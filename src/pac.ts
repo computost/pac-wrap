8cb3b67705bc3be3d2f539bd0dee31d0e3f3d062
melody-universe <tyler.hogsett@microsoft.com>
2021-10-04 21:27:59 -0700

    fixed linux exiting

--- src/pac.ts
+++ src/pac.ts
@@ -1,15 +1,11 @@
 import execa from "execa";
-import { stdout } from "process";
+import { stderr, stdout } from "process";
 import getPacPath from "./getPacPath.js";
 
 export default async function pac(...args: string[]) {
   const pacPath = await getPacPath();
   const process = execa(pacPath, args);
+  process.stderr?.pipe(stderr);
   process.stdout?.pipe(stdout);
-  const endPromise = new Promise((resolve) => {
-    process.addListener("exit", resolve);
-    process.addListener("close", resolve);
-    process.addListener("disconnect", resolve);
-  });
-  await Promise.race([process, endPromise]);
+  await process;
 }
