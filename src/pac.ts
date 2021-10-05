d4c165e5d25ad0ee0f42a31a468711a93ca57086
melody-universe <tyler.hogsett@microsoft.com>
2021-10-05 12:28:41 -0700

    explicitly handle piping of stdin

--- src/pac.ts
+++ src/pac.ts
@@ -1,11 +1,15 @@
 import execa from "execa";
-import { stderr, stdout } from "process";
+import { stderr, stdin, stdout } from "process";
 import getPacPath from "./getPacPath.js";
 
 export default async function pac(...args: string[]) {
   const pacPath = await getPacPath();
-  const process = execa(pacPath, args, { stdin: "ipc" });
+  const process = execa(pacPath, args);
   process.stdout!.pipe(stdout);
   process.stderr!.pipe(stderr);
+  stdin.pipe(process.stdin!);
+  process.on("close", () => {
+    stdin.unpipe(process.stdin!);
+  });
   await process;
 }
