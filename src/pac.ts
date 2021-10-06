786e248f119e71db6df1028a3a3de65ca4f5be2d
melody-universe <tyler.hogsett@microsoft.com>
2021-10-06 00:22:52 -0700

    testing if stdio ignore actually exits in linux in ADO

--- src/pac.ts
+++ src/pac.ts
@@ -4,14 +4,14 @@ import getPacPath from "./getPacPath.js";
 
 export default async function pac(...args: string[]) {
   const pacPath = await getPacPath();
-  const process = execa(pacPath, args, { detached: true });
-  process.stdout!.pipe(stdout);
+  const process = execa(pacPath, args, { stdio: "ignore" });
+  /*process.stdout!.pipe(stdout);
   process.stderr!.pipe(stderr);
   stdin.pipe(process.stdin!);
   process.on("close", () => {
     stdin.unpipe(process.stdin!);
   });
   stdin.unref();
-  process.unref();
+  process.unref();*/
   await process;
 }
